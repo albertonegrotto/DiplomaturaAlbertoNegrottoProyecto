0a032f877997d5c0154185731c2007d12dd6df0f
Alberto Negrotto <anegrotto@gmail.com>
2023-01-25 14:13:39 -0300

    Modulo 6 Unidad 3

--- MOD6UNI3/backend/routes/admin/clientes.js
+++ MOD6UNI3/backend/routes/admin/clientes.js
@@ -109,7 +109,6 @@ router.post('/modificar', async (req, res, next) => {
          else{
             console.log('No hay Archivos');
             imagen_id = img_id; 
-            // Carga la original
          }
       }
       if (borrar_img_vieja && req.body.img_original) {
